be1829c8612372ff920af1c3bf4b2b81e09b0fc1
moleium <molenoch@protonmail.com>
2024-10-03 21:55:25 +0300

    Refactor scheme to match grammar

--- queries/dascript/highlights.scm
+++ queries/dascript/highlights.scm
@@ -12,27 +12,28 @@
 (boolean_literal) @boolean
 
 ; Functions
-(function_definition
+(function_declaration
   "def" @keyword.function
   name: (identifier) @function)
 (function_call
   function: (identifier) @function.call)
 
 ; Structs and Enums
-(struct_definition
+(struct_declaration
   "struct" @keyword.storage.type
   name: (identifier) @type)
-(struct_definition
+(struct_declaration
   "class" @keyword.storage.type
   name: (identifier) @type)
+(enum_declaration "enum" @keyword.storage.type)
 
 ; Variable declarations
 (variable_declaration
   "let" @keyword
   name: (identifier) @variable)
-(variable_declaration
+(parameter
   "var" @keyword
-  name: (identifier) @variable)
+  name: (identifier) @parameter)
 
 ; Control flow
 "return" @keyword.return
@@ -40,6 +41,7 @@
 "else" @keyword.control.conditional
 "for" @keyword.control.repeat
 "while" @keyword.control.repeat
+"in" @keyword.control.repeat
 
 ; Modifiers
 "const" @keyword.storage.modifier
@@ -49,6 +51,7 @@
 "public" @keyword.storage.modifier
 "shared" @keyword.storage.modifier
 "static" @keyword.storage.modifier
+"sealed" @keyword.storage.modifier
 
 ; Parameters
 (parameter
@@ -70,9 +73,21 @@
 "||" @operator
 "!" @operator
 "." @operator
+"=" @operator
+":" @operator
+"[" @operator
+"]" @operator
+"(" @operator
+")"
+"<" @operator
+">"
+"," @operator
+";" @operator
+
 
 ((identifier) @type.builtin
  (#match? @type.builtin "^(bool|void|string|auto|int|int2|int3|int4|uint|bitfield|uint2|uint3|uint4|float|float2|float3|float4|range|urange|block|int64|uint64|double|function|lambda|int8|uint8|int16|uint16|tuple|variant|generator|yield)$"))
 
 (annotation
   name: (identifier) @attribute)
+"typedef" @keyword
